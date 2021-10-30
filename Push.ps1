e07141bb6ee842d809681bf9aab0afab8571f2fb
Andrew Piper <andrew@threax.com>
2021-10-30 06:38:50 -0400

    Add push script.

---
+++ Push.ps1
@@ -0,0 +1,8 @@
+git push
+git push --tags
+git push https://github.com/threax/Threax.AspNetCore.RazorExt.git
+git push https://github.com/threax/Threax.AspNetCore.RazorExt.git --tags
+git push https://gitlab.com/threax-software/Threax.AspNetCore.RazorExt.git
+git push https://gitlab.com/threax-software/Threax.AspNetCore.RazorExt.git --tags
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.AspNetCore.RazorExt
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.AspNetCore.RazorExt --tags
